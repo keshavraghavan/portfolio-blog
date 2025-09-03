c4f8c7ae030974d5cd77cbc51557e8824fa76614
Keshav Raghavan <raghavankeshav@gmail.com>
2025-09-02 21:30:05 -0500

    change of plans - spotify

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,11 +18,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-0 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums whitespace-nowrap">
+            <div className="w-full flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+              <p className="text-neutral-600 dark:text-neutral-400 w-32 tabular-nums whitespace-nowrap">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
